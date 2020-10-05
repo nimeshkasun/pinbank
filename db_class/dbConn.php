f4e39203d8bd7f8ff264b3f6cb81d8168b7cdf33
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-05 11:22:44 +0530

    View Prohibited

--- db_class/dbConn.php
+++ db_class/dbConn.php
@@ -3,6 +3,9 @@
 	$username = "root";
 	$password = "";
 	$database = "pinDB";
+	$username_online = "pinonlin_superadmin";
+	$password_online = "superKDU2020";
+	$database_online = "pinonlin_pindb";
 	if(mysqli_connect($servername, $username, $password, $database))
 	{
 		$conn = mysqli_connect($servername, $username, $password, $database);
@@ -10,10 +13,17 @@
 			echo $success."(1) Connect to DB"."<br><br>";
 		}
 	}
+	else if(mysqli_connect($servername, $username_online, $password_online, $database_online))
+	{
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		if(isset($_GET['execute'])){
+			echo $success."(2) Connect to DB"."<br><br>";
+		}
+	}
 	else
 	{
 		if(isset($_GET['execute'])){
-			echo $bug."(2) Connect to DB: " . mysqli_connect_errno()."<br><br>";
+			echo $bug."(3) Connect to DB: " . mysqli_connect_errno()."<br><br>";
 		}
 	}
 ?>
