a0617be25e31479cf21bbc1c32ef9cfad90cbbae
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-05 11:43:38 +0530

    Update dbClass.php

--- db_class/dbClass.php
+++ db_class/dbClass.php
@@ -21,25 +21,33 @@ if(isset($_GET['execute'])){
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// (1) Connect to DB
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "pinDB";
-if(mysqli_connect($servername, $username, $password, $database))
-{
-	$conn = mysqli_connect($servername, $username, $password, $database);
-	if(isset($_GET['execute'])){
-		echo $sucess."(1) Connect to DB"."<br><br>";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "pinDB";
+	$username_online = "pinonlin_superadmin";
+	$password_online = "superKDU2020";
+	$database_online = "pinonlin_pindb";
+	if(mysqli_connect($servername, $username, $password, $database))
+	{
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		if(isset($_GET['execute'])){
+			echo $success."(1) Connect to DB"."<br><br>";
+		}
 	}
-}
-else
-{
-	if(isset($_GET['execute'])){
-		echo $bug."(2) Connect to DB: " . mysqli_connect_errno()."<br><br>";
+	else if(mysqli_connect($servername, $username_online, $password_online, $database_online))
+	{
+		$conn = mysqli_connect($servername, $username_online, $password_online, $database_online);
+		if(isset($_GET['execute'])){
+			echo $success."(2) Connect to DB"."<br><br>";
+		}
 	}
-}*/
-
-require_once './dbConn.php';
+	else
+	{
+		if(isset($_GET['execute'])){
+			echo $bug."(3) Connect to DB: " . mysqli_connect_errno()."<br><br>";
+		}
+	}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// (2) tblUserDetails
 
