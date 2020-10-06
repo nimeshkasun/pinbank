7d2378a6032ea0f85f1f552c8518618dc5d42252
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-07 00:00:02 +0530

    Update mail.php

--- db_class/mail/mail.php
+++ db_class/mail/mail.php
@@ -5,8 +5,11 @@ $mail->isSMTP();
 $mail->SMTPDebug = 0;
 $mail->Debugoutput = 'html';
 $mail->Host = 'smtp.gmail.com';
+/*$mail->Port = 587;*/
 $mail->Port = 465;
-$mail->SMTPSecure = 'tls';
+/*$mail->SMTPSecure = 'tls';*/
+/*$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;*/
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->SMTPAuth = true;
 require_once 'emailpass.php';
 //TO
