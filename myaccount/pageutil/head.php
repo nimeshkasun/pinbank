90ada4a4d2bed799d1f71ce4b7ac5393984cafca
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-06 20:57:33 +0530

    temp

--- myaccount/pageutil/head.php
+++ myaccount/pageutil/head.php
@@ -5,55 +5,13 @@
     <meta name="description" content="#">
     <meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
     <meta name="author" content="#">
-    <!-- Favicon icon -->
-    <link rel="icon" href="./files/assets/images/favicon.ico" type="image/x-icon">
-    <!-- Google font-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-    <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="./files/bower_components/bootstrap/css/bootstrap.min.css">
-    <!-- feather Awesome -->
-    <link rel="stylesheet" type="text/css" href="./files/assets/icon/feather/css/feather.css">
-    <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="./files/assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="./files/assets/css/jquery.mCustomScrollbar.css">
-    <!-- themify-icons line icon -->
-    <link rel="stylesheet" type="text/css" href="./files/assets/icon/themify-icons/themify-icons.css">
-    <!-- ico font -->
-    <link rel="stylesheet" type="text/css" href="./files/assets/icon/icofont/css/icofont.css">
-    <!-- Notification.css -->
-    <link rel="stylesheet" type="text/css" href="./files/assets/pages/notification/notification.css">
-    <!-- Animate.css -->
-    <link rel="stylesheet" type="text/css" href="./files/bower_components/animate.css/css/animate.css">
-    <link rel="stylesheet" type="text/css" href="./files/assets/css/linearicons.css">
-    <!-- Helps in automatic retrival -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
-        <script src="./files/assets/jquery.min.js"></script>
-        <script src="./files/assets/bootstrap.min.js"></script>
-        <!-- Data Table Css -->
-        <link rel="stylesheet" type="text/css" href="./files/bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-        <link rel="stylesheet" type="text/css" href="./files/assets/pages/data-table/css/buttons.dataTables.min.css">
-        <link rel="stylesheet" type="text/css" href="./files/bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css">
-        <!-- jpro forms css -->
-        <link rel="stylesheet" type="text/css" href="./files/assets/pages/j-pro/css/demo.css">
-        <link rel="stylesheet" type="text/css" href="./files/assets/pages/j-pro/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="./files/assets/pages/j-pro/css/j-forms.css">
-        <link rel="stylesheet" type="text/css" href="./files/assets/pages/j-pro/css/j-pro-modern.css">
-
-        <!-- Select 2 css -->
-        <link rel="stylesheet" href="./files/bower_components/select2/css/select2.min.css">
-        <!-- Multi Select css -->
-        <link rel="stylesheet" type="text/css" href="./files/bower_components/bootstrap-multiselect/css/bootstrap-multiselect.css">
-        <link rel="stylesheet" type="text/css" href="./files/bower_components/multiselect/css/multi-select.css">
-        <!-- sweet alert framework -->
-        <link rel="stylesheet" type="text/css" href="./files/bower_components/sweetalert/css/sweetalert.css">
-        <!-- animation nifty modal window effects css -->
-        <link rel="stylesheet" type="text/css" href="./files/assets/css/component.css">
+    
 
         <?php
         ob_start();
 session_start();
             require_once './dbClass.php';
+            echo "string";
             print_r($_SESSION);
         ?>
         <!-- Lock Screen popup -->
