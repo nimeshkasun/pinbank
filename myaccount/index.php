90015da4a3f1fff2133b5a4a76f8d3de5f3382e8
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-06 21:07:55 +0530

    Update index.php

--- myaccount/index.php
+++ myaccount/index.php
@@ -3,34 +3,3 @@ ob_start();
 session_start();
 print_r($_SESSION);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <?php require_once './pageutil/head.php'; ?>
-    </head>
-    <body>
-        <?php require_once './pageutil/preloader.php'; ?>
-        <div id="pcoded" class="pcoded">
-            <div class="pcoded-overlay-box"></div>
-                <div class="pcoded-container navbar-wrapper">
-                    <?php require_once './pageutil/headerbar.php'; ?>
-                    <div class="pcoded-main-container">
-                        <div class="pcoded-wrapper">
-                        <?php require_once './pageutil/navigation.php'; ?>
-                            <div class="pcoded-content">
-                                <div class="pcoded-inner-content">
-                                    <div class="main-body">
-                                        <div class="page-wrapper">
-                                            <?php require_once './pageutil/body_dashboard.php'; ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php require_once './pageutil/footer.php'; ?>
-    </body>
-</html>
