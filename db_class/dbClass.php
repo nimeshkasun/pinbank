71fa9a4e35e1e8d2ddc7021a6389cab52e064947
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-06 18:14:06 +0530

    Update dbClass.php

--- db_class/dbClass.php
+++ db_class/dbClass.php
@@ -272,7 +272,7 @@ if ($result->num_rows > 0) {
 						//My Account Part - End
 					if($userTypesaved == "custAdv" || $userTypesaved == "custMed" || $userTypesaved == "custEas"){
 						$_SESSION['chatEmail'] = $emailsaved;
-						header('location: ../myaccount/');
+						this.header('location: ../myaccount/');
 						/*echo "<script> location.replace('../myaccount/'); </script>";*/
 					}elseif($userTypesaved == "staAdmin" || $userTypesaved == "staLocal" || $userTypesaved == "staSupp"){
 						/*header('location: ../admin/');*/
