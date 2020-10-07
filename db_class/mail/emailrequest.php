f35a65df0cdc83d03249dd7f8cd2c49b3972e9a1
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-07 20:39:26 +0530

    Update emailrequest.php

--- db_class/mail/emailrequest.php
+++ db_class/mail/emailrequest.php
@@ -7,7 +7,7 @@
 	/*$mail->Host = 'smtp.gmail.com';*/
 	$mail->Host = 'mail.pinbank.live';
 	/*$mail->Port = 587;*/
-	$mail->Port = 587;
+	$mail->Port = 465;
 	$mail->SMTPSecure = 'tls';
 	$mail->SMTPAuth = true;
 	require_once 'emailpass.php';
