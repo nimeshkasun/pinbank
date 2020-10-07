692a38b51c9314633befedab01d3322b40d9ca04
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-10-07 20:09:48 +0530

    Mail check

--- db_class/mail/emailpass.php
+++ db_class/mail/emailpass.php
@@ -2,8 +2,8 @@
 
 //Don't change --start
 	//Username to use for SMTP authentication - use full email address for gmail
-	$mail->Username = "pinonlinebanking";
-	$Username = "pinonlinebanking";
+	$mail->Username = "support@pinbank.live";
+	$Username = "support@pinbank.live";
 	
 	//Password to use for SMTP authentication
 	$mail->Password = "Pin@2020";
