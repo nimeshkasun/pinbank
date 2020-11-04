75eefdc801ac5c48cfc36c8c5eef4a83a290c143
Nimesh Kasun <nimesh.ekanayaka7@gmail.com>
2020-11-04 19:12:14 +0530

    Update testmsg.php

--- testmsg.php
+++ testmsg.php
@@ -8,13 +8,7 @@ $Message->body = 'Pin Online Bank System Check: 1';
 
 $MessageBird->messages->create($Message);*/
 
- 		/*include("smsn/newsletterslk.class.php");
-        $mysms=new Newsletterslk;
-        
-        $mysms->setUser('5iAhRRFK5fRARQ6xDOofTkGCDtLxWU0W','B3xD1604488868');// Initializing User Api Key and Api Token
-        $mysms->setSenderID('DEMO_SMS');// Initializing Sender ID Default Web SMS
-        $mysms->msgType='sms';// Initializing Message Type
-		$mysms->SendMessage('0718810575','Pin Test');*/
+ 		
 
 		$smsMessage = "Test Message";
 		require_once 'sms/send.php';
